b1a371231b4dc749ae0970ce0b5dd5dd08314d5b
Mizux Seiha <mizux.dev@gmail.com>
2020-01-30 23:01:05 +0100

    Add files

--- js/deps.sh
+++ js/deps.sh
@@ -8,6 +8,9 @@ wget -N --trust-server-name https://unpkg.com/three/build/three
 wget -N --trust-server-name https://unpkg.com/three/build/three.module
 
 wget -N --trust-server-name https://unpkg.com/dat.gui/build/dat.gui
+
+wget -N --trust-server-name https://raw.githubusercontent.com/jonobr1/two.js/master/build/two.js
+
 #wget -N --trust-server-name https://unpkg.com/dat.gui/build/dat.gui.module
 
 #wget -N --trust-server-name https://unpkg.com/chevrotain/lib/chevrotain
