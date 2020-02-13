6f7944f14fbd5ac04da088dcbbb4e638ff6fe585
Mizux Seiha <mizux.dev@gmail.com>
2020-02-13 19:30:29 +0100

    Add download button

--- genapp/scripts/main.js
+++ genapp/scripts/main.js
@@ -109,3 +109,48 @@ var sizeGUI = atlasGUI.addFolder("Size");
 //};
 //update();
 
+// Download button stuff
+var btn = document.querySelector('button');
+//var canvas = document.querySelector('canvas');
+
+function triggerDownload (imgURI) {
+  var evt = new MouseEvent('click', {
+    view: window,
+    bubbles: false,
+    cancelable: true
+  });
+
+  var a = document.createElement('a');
+  a.setAttribute('download', `${theme.name}_${atlas.name}.png`);
+  a.setAttribute('href', imgURI);
+  a.setAttribute('target', '_blank');
+
+  a.dispatchEvent(evt);
+}
+
+btn.addEventListener('click', function () {
+  var canvas = document.getElementById('canvas');
+  var ctx = canvas.getContext('2d');
+
+  var svg = document.querySelector('svg');
+  var data = (new XMLSerializer()).serializeToString(svg);
+  var DOMURL = window.URL || window.webkitURL || window;
+
+  var img = new Image();
+  var svgBlob = new Blob([data], {type: 'image/svg+xml;charset=utf-8'});
+  var url = DOMURL.createObjectURL(svgBlob);
+
+  img.onload = function () {
+    ctx.drawImage(img, 0, 0);
+    DOMURL.revokeObjectURL(url);
+
+    var imgURI = canvas
+        .toDataURL('image/png')
+        .replace('image/png', 'image/octet-stream');
+
+    triggerDownload(imgURI);
+  };
+
+  img.src = url;
+});
+
