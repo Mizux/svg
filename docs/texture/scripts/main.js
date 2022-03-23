e5f24c09067fe1ce81056ba83a240e36e75d8978
Mizux Seiha <mizux.dev@gmail.com>
2022-03-23 23:36:44 +0100

    update texture

--- docs/texture/scripts/main.js
+++ docs/texture/scripts/main.js
@@ -1,9 +1,12 @@
+import { default as dat } from "./dat.gui.module.js";
+
 // SVG struct
-var svg = new SVG();
+import { SVG } from "./svg.js";
+const svg = new SVG();
 
 function updateGUI() {
-  for (i = 0; i < arguments.length; i++) {
-    for (var j in arguments[i].__controllers) {
+  for (let i = 0; i < arguments.length; i++) {
+    for (let j in arguments[i].__controllers) {
       arguments[i].__controllers[j].updateDisplay();
     }
   }
@@ -14,15 +17,15 @@ function redraw() {
 }
 
 // First define Dat.Gui instances
-var svgGUI = new dat.GUI({ load: JSON });
+const svgGUI = new dat.GUI({ load: JSON });
 // must be call before gui construction
-svgGUI.remember(svg, 'Svg');
+//svgGUI.remember(svg, 'Svg');
 svgGUI.remember(svg.pattern, 'Pattern');
 svgGUI.remember(svg.size, 'Size');
 svgGUI.remember(svg.colors, 'Color Palette');
 svgGUI.remember(svg.filters, 'Filters');
 
-var patternGUI = svgGUI.addFolder("Pattern");
+const patternGUI = svgGUI.addFolder("Pattern");
 {
   patternGUI.add(svg.pattern, "name", svg.getPatternList()).onChange(setPattern);
   patternGUI.open();
@@ -34,23 +37,23 @@ function setPattern() {
   redraw();
 }
 
-var sizeGUI = svgGUI.addFolder("Size");
+const sizeGUI = svgGUI.addFolder("Size");
 {
   sizeGUI.add(svg.size, "width", 64, 1024, 64).onChange(redraw);
   sizeGUI.add(svg.size, "height", 64, 1024, 64).onChange(redraw);
   sizeGUI.open();
 }
 
-var colorPaletteGUI = svgGUI.addFolder("Color Palette");
+const colorPaletteGUI = svgGUI.addFolder("Color Palette");
 {
   colorPaletteGUI.addColor(svg.colors, "fg").onChange(redraw);
   colorPaletteGUI.addColor(svg.colors, "bg").onChange(redraw);
   colorPaletteGUI.open();
 }
 
-var filterGUI = svgGUI.addFolder("Filters");
+const filterGUI = svgGUI.addFolder("Filters");
 {
-  var paperGUI = filterGUI.addFolder("Paper");
+  const paperGUI = filterGUI.addFolder("Paper");
   paperGUI.add(svg.filters.paper, "seed", 0, 8).onChange(redraw);
   paperGUI.add(svg.filters.paper, "numOctaves", 2, 8, 1).onChange(redraw);
   paperGUI.add(svg.filters.paper, "baseFrequency", 0.00001, 0.7).onChange(redraw);
@@ -58,7 +61,7 @@ var filterGUI = svgGUI.addFolder("Filters");
   //paperGUI.open();
 }
 {
-  var woodGUI = filterGUI.addFolder("Wood");
+  const woodGUI = filterGUI.addFolder("Wood");
   woodGUI.add(svg.filters.wood, "seed", 0, 8).onChange(redraw);
   woodGUI.add(svg.filters.wood, "numOctaves", 2, 8, 1).onChange(redraw);
   woodGUI.add(svg.filters.wood, "baseFrequency", 0.00001, 0.7).onChange(redraw);
@@ -66,7 +69,7 @@ var filterGUI = svgGUI.addFolder("Filters");
   //woodGUI.open();
 }
 {
-  var noise_oneGUI = filterGUI.addFolder("Noise1");
+  const noise_oneGUI = filterGUI.addFolder("Noise1");
   noise_oneGUI.add(svg.filters.noise_one, "seed", 0, 8).onChange(redraw);
   noise_oneGUI.add(svg.filters.noise_one, "numOctaves", 1, 8, 1).onChange(redraw);
   noise_oneGUI.add(svg.filters.noise_one, "baseFrequency", 0.00001, 0.7).onChange(redraw);
@@ -78,7 +81,7 @@ var filterGUI = svgGUI.addFolder("Filters");
   //noise_oneGUI.open();
 }
 {
-  var noise_twoGUI = filterGUI.addFolder("Noise2");
+  const noise_twoGUI = filterGUI.addFolder("Noise2");
   noise_twoGUI.add(svg.filters.noise_two, "seed", 0, 8).onChange(redraw);
   noise_twoGUI.add(svg.filters.noise_two, "numOctaves", 1, 8, 1).onChange(redraw);
   noise_twoGUI.add(svg.filters.noise_two, "baseFrequency", 0.00001, 0.7).onChange(redraw);
@@ -92,39 +95,39 @@ var filterGUI = svgGUI.addFolder("Filters");
 
 // Download button stuff
 function triggerDownload (imgURI) {
-  var evt = new MouseEvent('click', {
+  const evt = new MouseEvent('click', {
     view: window,
     bubbles: false,
     cancelable: true
   });
 
-  var a = document.createElement('a');
-  a.setAttribute('download', `${svg.pattern}_${svg.size.width}x${svg.size.height}.png`);
+  const a = document.createElement('a');
+  a.setAttribute('download', `${svg.pattern.name}_${svg.size.width}x${svg.size.height}.png`);
   a.setAttribute('href', imgURI);
   a.setAttribute('target', '_blank');
 
   a.dispatchEvent(evt);
 }
 
-document.getElementById("btn-png").addEventListener('click', function () {
-  var canvas = document.getElementById('canvas');
+function save_to_png() {
+  const canvas = document.getElementById('canvas');
   canvas.setAttribute('width', `${svg.size.width}`); // clears the canvas
   canvas.setAttribute('height', `${svg.size.height}`); // clears the canvas
-  var ctx = canvas.getContext('2d');
+  const ctx = canvas.getContext('2d');
 
-  var svg = document.querySelector('svg');
-  var data = (new XMLSerializer()).serializeToString(svg);
-  var DOMURL = window.URL || window.webkitURL || window;
+  const svg_obj = document.querySelector('svg');
+  const data = (new XMLSerializer()).serializeToString(svg_obj);
+  const DOMURL = window.URL || window.webkitURL || window;
 
-  var img = new Image();
-  var svgBlob = new Blob([data], {type: 'image/svg+xml;charset=utf-8'});
-  var url = DOMURL.createObjectURL(svgBlob);
+  const img = new Image();
+  const svgBlob = new Blob([data], {type: 'image/svg+xml;charset=utf-8'});
+  const url = DOMURL.createObjectURL(svgBlob);
 
   img.onload = function () {
     ctx.drawImage(img, 0, 0);
     DOMURL.revokeObjectURL(url);
 
-    var imgURI = canvas
+    const imgURI = canvas
         .toDataURL('image/png')
         .replace('image/png', 'image/octet-stream');
 
@@ -132,12 +135,13 @@ document.getElementById("btn-png").addEventListener('click', function () {
   };
 
   img.src = url;
-});
+}
 
+document.getElementById("btn-png").addEventListener('click', save_to_png);
 
-document.getElementById("btn-svg").addEventListener('click', function () {
-  const svg = document.querySelector('svg');
-  const data = (new XMLSerializer()).serializeToString(svg);
+function save_to_svg() {
+  const svg_obj = document.querySelector('svg');
+  const data = (new XMLSerializer()).serializeToString(svg_obj);
   const DOMURL = window.URL || window.webkitURL || window;
 
   const svgBlob = new Blob([data], {type:"image/svg+xml;charset=utf-8"});
@@ -150,11 +154,13 @@ document.getElementById("btn-svg").addEventListener('click', function () {
   });
 
   const a = document.createElement('a');
-  a.setAttribute('download', `${svg.pattern}_${svg.size.width}x${svg.size.height}.svg`);
+  a.setAttribute('download', `${svg.pattern.name}_${svg.size.width}x${svg.size.height}.svg`);
   a.setAttribute('href', svgUrl);
   a.setAttribute('target', '_blank');
 
   a.dispatchEvent(evt);
-});
+}
+
+document.getElementById("btn-svg").addEventListener('click', save_to_svg);
 
 redraw();
--- docs/texture/scripts/svg.js
+++ docs/texture/scripts/svg.js
@@ -1,53 +1,54 @@
-var SVG = function() {
-  const pattern = {name: 'paper'};
+class SVG {
+  constructor() {
+    // Public Members
+    this.pattern = {
+      name: 'paper'
+    };
+    this.size = {
+      width: 512,
+      height: 512,
+    };
+    this.colors = {
+      fg: '#f1f1f1',
+      bg: '#101010'
+    };
+    this.filters = {
+      paper: {
+        seed: 0,
+        numOctaves: 4,
+        baseFrequency: 0.064,
+        blendMode: "multiply",
+      },
+      wood: {
+        seed: 0,
+        numOctaves: 4,
+        baseFrequency: 0.064,
+        blendMode: "multiply",
+      },
+      noise_one: {
+        seed: 0,
+        numOctaves: 1,
+        baseFrequency: 0.3,
+        blendMode: "normal",
+        k1: 2.44,
+        k2: 0.5,
+        k3: 0.5,
+        k4: -0.15,
+      },
+      noise_two: {
+        seed: 0,
+        numOctaves: 1,
+        baseFrequency: 0.3,
+        blendMode: "normal",
+        k1: 8.0,
+        k2: 0.0,
+        k3: 2.0,
+        k4: 0.0,
+      },
+    };
+  }
 
-  const size = {
-    width: 512,
-    height: 512,
-  };
-
-  const colors = {
-    fg: '#f1f1f1',
-    bg: '#101010'
-  };
-
-  const filters = {
-    paper: {
-      seed: 0,
-      numOctaves: 4,
-      baseFrequency: 0.064,
-      blendMode: "multiply",
-    },
-    wood: {
-      seed: 0,
-      numOctaves: 4,
-      baseFrequency: 0.064,
-      blendMode: "multiply",
-    },
-    noise_one: {
-      seed: 0,
-      numOctaves: 1,
-      baseFrequency: 0.3,
-      blendMode: "normal",
-      k1: 2.44,
-      k2: 0.5,
-      k3: 0.5,
-      k4: -0.15,
-    },
-    noise_two: {
-      seed: 0,
-      numOctaves: 1,
-      baseFrequency: 0.3,
-      blendMode: "normal",
-      k1: 8.0,
-      k2: 0.0,
-      k3: 2.0,
-      k4: 0.0,
-    },
-  };
-
-
-  function getPatternList() {
+  getPatternList() {
     return [
       "paper",
       "wood",
@@ -56,7 +57,7 @@ var SVG = function() {
     ];
   }
 
-  function getBlendModeList() {
+  getBlendModeList() {
     return [
       "color",
       "color-burn",
@@ -77,203 +78,187 @@ var SVG = function() {
     ];
   }
 
-  function svgHeader() {
-    return `
-<svg
-  viewBox="0 0 ${size.width} ${size.height}"
-  width="${size.width}" height="${size.height}"
-  xmlns="http://www.w3.org/2000/svg"
-  xmlns:xlink="http://www.w3.org/1999/xlink">`;
-  }
-
-  function svgFooter() {
-    return `</svg>`;
-  }
-
-  function printPaperFilter() {
-    return `
-<filter id="paper" x="0%" y="0%" width="100%" height="100%">
-  <feTurbulence
-    type="fractalNoise"
-    seed="${filters.paper.seed}"
-    numOctaves="${filters.paper.numOctaves}"
-    baseFrequency="${filters.paper.baseFrequency}"
-    stitchTiles="stitch"
-    result="noise"
-  />
-  <feDiffuseLighting
-    surfaceScale="2"
-    lighting-color=" ${colors.light} "
-    in="noise"
-    result="diffLight">
-    <feDistantLight elevation="33" azimuth="45"/>
-  </feDiffuseLighting>
-  <feComposite
-    operator="in"
-    in2="SourceGraphic" result="mask"
-  />
-  <feBlend
-    mode="${filters.paper.blendMode}"
-    in="mask" in2="SourceGraphic" result="result"
-  />
-</filter>
-`;
-  }
-
-  function printWoodFilter() {
-    return `
-<filter id="wood" x="0%" y="0%" width="100%" height="100%">
-  <feTurbulence
-    type="fractalNoise"
-    seed="${filters.wood.seed}"
-    numOctaves="${filters.wood.numOctaves}"
-    baseFrequency="${filters.wood.baseFrequency}"
-    stitchTiles="stitch"
-    result="noise"
-  />
-  <feDiffuseLighting
-    surfaceScale="2"
-    lighting-color=" ${colors.light} "
-    in="noise"
-    result="diffLight">
-    <feDistantLight elevation="33" azimuth="45"/>
-  </feDiffuseLighting>
-  <feComposite
-    operator="in"
-    in2="SourceGraphic" result="mask"
-  />
-  <feBlend
-    mode="${filters.wood.blendMode}"
-    in="mask" in2="SourceGraphic" result="result"
-  />
-</filter>
-`;
-  }
-
-  function printNoise1Filter() {
-    return `
-<filter id="noise_one" x="0%" y="0%" width="100%" height="100%"
-  style="color-interpolation-filters:sRGB;">
-  <feTurbulence
-     type="turbulence"
-     seed="${filters.noise_one.seed}"
-     numOctaves="${filters.noise_one.numOctaves}"
-     baseFrequency="${filters.noise_one.baseFrequency}"
-     stitchTiles="stitch"
-     result="noise"
-  />
-  <feColorMatrix
-     type="saturate"
-     values="0"
-     result="colorMatrix"
-  />
-  <feComposite
-     operator="arithmetic"
-     k1="${filters.noise_one.k1}"
-     k2="${filters.noise_one.k2}"
-     k3="${filters.noise_one.k3}"
-     k4="${filters.noise_one.k4}"
-     in="SourceGraphic" in2="ColorMatrix" result="mask"
-  />
-  <feBlend
-     mode="${filters.noise_one.blendMode}"
-     in="mask" in2="SourceGraphic" result="blend"
-  />
-  <feComposite
-     operator="in"
-     in="blend"
-     in2="SourceGraphic"
-     result="result"
-  />
-</filter>
-`;
-  }
-
-  function printNoise2Filter() {
-    return `
-<filter id="noise_two" x="0%" y="0%" width="100%" height="100%"
-  style="color-interpolation-filters:sRGB;">
-  <feTurbulence
-     type="turbulence"
-     seed="${filters.noise_two.seed}"
-     numOctaves="${filters.noise_two.numOctaves}"
-     baseFrequency="${filters.noise_two.baseFrequency}"
-     stitchTiles="stitch"
-     result="noise"
-  />
-  <feColorMatrix
-     type="saturate"
-     values="0"
-     result="colorMatrix"
-  />
-  <feComposite
-     operator="arithmetic"
-     k1="${filters.noise_two.k1}"
-     k2="${filters.noise_two.k2}"
-     k3="${filters.noise_two.k3}"
-     k4="${filters.noise_two.k4}"
-     in="SourceGraphic" in2="ColorMatrix" result="mask"
-  />
-  <feBlend
-     mode="${filters.noise_two.blendMode}"
-     in="mask" in2="SourceGraphic" result="blend"
-  />
-  <feComposite
-     operator="in"
-     in="blend"
-     in2="SourceGraphic"
-     result="result"
-  />
-</filter>
-`;
-  }
-
-
-  function drawPattern() {
-    return `
-<rect
-  x="0" y="0" width="100%" height="100%"
-  fill=" ${colors.fg} "
-  filter="url(#${pattern.name})"
-/>
-`;
-  }
-
-  function print() {
-    let res = svgHeader();
-    res += `<defs>`;
-    switch(pattern.name) {
+  print() {
+    let res = this.#svgHeader();
+    res += `<defs>\n`;
+    switch(this.pattern.name) {
       case 'paper':
-        res += printPaperFilter();
+        res += this.#printPaperFilter();
         break;
       case 'wood':
-        res += printWoodFilter();
+        res += this.#printWoodFilter();
         break;
       case 'noise_one':
-        res += printNoise1Filter();
+        res += this.#printNoise1Filter();
         break;
       case 'noise_two':
-        res += printNoise2Filter();
+        res += this.#printNoise2Filter();
         break;
       default:
-        console.log('Sorry, pattern \"' + pattern.name + '\" unknown.');
+        console.log('Sorry, pattern \"' + this.pattern.name + '\" unknown.');
     }
-    res += `</defs>`;
-    res += drawPattern();
-    res += svgFooter();
+    res += `</defs>\n`;
+    res += this.#drawPattern();
+    res += this.#svgFooter();
     return res;
   }
 
-  // SVG Module
-  return {
-    // Public Members
-    pattern: pattern,
-    size: size,
-    colors: colors,
-    filters: filters,
-    // Public Methods
-    print: print,
-    getBlendModeList: getBlendModeList,
-    getPatternList: getPatternList,
-  };
+  #printPaperFilter() {
+    return (
+      `<filter id="paper" x="0%" y="0%" width="100%" height="100%">\n` +
+      ` <feTurbulence` +
+      ` type="fractalNoise"` +
+      ` seed="${this.filters.paper.seed}"` +
+      ` numOctaves="${this.filters.paper.numOctaves}"` +
+      ` baseFrequency="${this.filters.paper.baseFrequency}"` +
+      ` stitchTiles="stitch"` +
+      ` result="noise"` +
+      `/>\n` +
+      ` <feDiffuseLighting` +
+      ` surfaceScale="2"` +
+      ` lighting-color="${this.colors.bg}"` +
+      ` in="noise"` +
+      ` result="diffLight">\n` +
+      `  <feDistantLight elevation="33" azimuth="45"/>\n` +
+      ` </feDiffuseLighting>\n` +
+      ` <feComposite` +
+      ` operator="in"` +
+      ` in2="SourceGraphic" result="mask"` +
+      `/>\n` +
+      ` <feBlend` +
+      ` mode="${this.filters.paper.blendMode}"` +
+      ` in="mask" in2="SourceGraphic" result="result"` +
+      `/>\n` +
+      `</filter>\n`);
+  }
+
+  #printWoodFilter() {
+    return (
+      `<filter id="wood" x="0%" y="0%" width="100%" height="100%">\n` +
+      ` <feTurbulence` +
+      ` type="fractalNoise"` +
+      ` seed="${this.filters.wood.seed}"` +
+      ` numOctaves="${this.filters.wood.numOctaves}"` +
+      ` baseFrequency="${this.filters.wood.baseFrequency}"` +
+      ` stitchTiles="stitch"` +
+      ` result="noise"` +
+      `/>\n` +
+      ` <feDiffuseLighting` +
+      ` surfaceScale="2"` +
+      ` lighting-color="${this.colors.bg}"` +
+      ` in="noise"` +
+      ` result="diffLight">\n` +
+      `  <feDistantLight elevation="33" azimuth="45"/>\n` +
+      ` </feDiffuseLighting>\n` +
+      ` <feComposite` +
+      ` operator="in"` +
+      ` in2="SourceGraphic" result="mask"` +
+      `/>\n` +
+      ` <feBlend` +
+      ` mode="${this.filters.wood.blendMode}"` +
+      ` in="mask" in2="SourceGraphic" result="result"` +
+      `/>\n` +
+      `</filter>\n`);
+  }
+
+  #printNoise1Filter() {
+    return (
+      `<filter id="noise_one" x="0%" y="0%" width="100%" height="100%"` +
+      ` style="color-interpolation-filters:sRGB;">\n` +
+      ` <feTurbulence` +
+      ` type="turbulence"` +
+      ` seed="${this.filters.noise_one.seed}"` +
+      ` numOctaves="${this.filters.noise_one.numOctaves}"` +
+      ` baseFrequency="${this.filters.noise_one.baseFrequency}"` +
+      ` stitchTiles="stitch"` +
+      ` result="noise"` +
+      `/>\n` +
+      ` <feColorMatrix` +
+      ` type="saturate"` +
+      ` values="0"` +
+      ` result="colorMatrix"` +
+      `/>\n` +
+      ` <feComposite` +
+      ` operator="arithmetic"` +
+      ` k1="${this.filters.noise_one.k1}"` +
+      ` k2="${this.filters.noise_one.k2}"` +
+      ` k3="${this.filters.noise_one.k3}"` +
+      ` k4="${this.filters.noise_one.k4}"` +
+      ` in="SourceGraphic" in2="ColorMatrix" result="mask"` +
+      `/>\n` +
+      ` <feBlend` +
+      ` mode="${this.filters.noise_one.blendMode}"` +
+      ` in="mask" in2="SourceGraphic" result="blend"` +
+      `/>\n` +
+      ` <feComposite` +
+      ` operator="in"` +
+      ` in="blend"` +
+      ` in2="SourceGraphic"` +
+      ` result="result"` +
+      `/>\n` +
+      `</filter>\n`);
+  }
+
+  #printNoise2Filter() {
+    return (
+      `<filter id="noise_two" x="0%" y="0%" width="100%" height="100%"` +
+      ` style="color-interpolation-filters:sRGB;">\n` +
+      ` <feTurbulence` +
+      ` type="turbulence"` +
+      ` seed="${this.filters.noise_two.seed}"` +
+      ` numOctaves="${this.filters.noise_two.numOctaves}"` +
+      ` baseFrequency="${this.filters.noise_two.baseFrequency}"` +
+      ` stitchTiles="stitch"` +
+      ` result="noise"` +
+      `/>\n` +
+      ` <feColorMatrix` +
+      ` type="saturate"` +
+      ` values="0"` +
+      ` result="colorMatrix"` +
+      `/>\n` +
+      ` <feComposite` +
+      ` operator="arithmetic"` +
+      ` k1="${this.filters.noise_two.k1}"` +
+      ` k2="${this.filters.noise_two.k2}"` +
+      ` k3="${this.filters.noise_two.k3}"` +
+      ` k4="${this.filters.noise_two.k4}"` +
+      ` in="SourceGraphic" in2="ColorMatrix" result="mask"` +
+      `/>\n` +
+      ` <feBlend` +
+      ` mode="${this.filters.noise_two.blendMode}"` +
+      ` in="mask" in2="SourceGraphic" result="blend"` +
+      `/>\n` +
+      ` <feComposite` +
+      ` operator="in"` +
+      ` in="blend"` +
+      ` in2="SourceGraphic"` +
+      ` result="result"` +
+      `/>\n` +
+      `</filter>\n`);
+  }
+
+  #drawPattern() {
+    return (
+      `<rect` +
+      ` x="0" y="0" width="100%" height="100%"` +
+      ` fill="${this.colors.fg}"` +
+      ` filter="url(#${this.pattern.name})"` +
+      `/>\n`);
+  }
+
+  #svgHeader() {
+    return (
+      `<svg` +
+      ` viewBox="0 0 ${this.size.width} ${this.size.height}"` +
+      ` width="${this.size.width}" height="${this.size.height}"` +
+      ` xmlns="http://www.w3.org/2000/svg"` +
+      ` xmlns:xlink="http://www.w3.org/1999/xlink"` +
+      `>\n`);
+  }
+
+  #svgFooter() {
+    return `</svg>`;
+  }
 }
+
+export { SVG };
