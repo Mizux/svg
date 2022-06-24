1552e48cf84b4675a6d98d27c87ad15db4b44e3f
Corentin Le Molgat <corentinl@google.com>
2022-06-24 15:30:46 +0200

    dev: rework docker management

--- docs/serve.sh
+++ docs/serve.sh
@@ -1,7 +1,13 @@
-#!/usr/bin/env sh
-set -e
+#!/usr/bin/env bash
+set -euo pipefail
 
 command -v docker
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 docker build -t svg .
-docker run --rm --net=host --init --name svg -it svg:latest
+docker run --rm --init --net=host \
+  -v ${SCRIPT_DIR}:/home/node/app \
+  -it \
+  --name svg \
+  svg:latest
